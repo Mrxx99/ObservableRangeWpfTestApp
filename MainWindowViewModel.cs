a97974d4a7e1c6938f22fa81ffb8382364c32c49
Mrxx99 <Mrxx99@googlemail.com>
2024-08-16 18:48:30 +0200

    Added sorting, grouping and filtering switches

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,9 +1,21 @@
-﻿using System.Windows.Data;
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Windows.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using ObservableRange;
 
 namespace ObservableRangeWpfTestApp;
+
+public record Item(int Value)
+{
+    public string BaseName { get; } = "Item";
+    public int Tens { get; } = (Value / 10) * 10;
+
+    public List<int> ReplaceCounters { get; set; } = [];
+
+    public override string ToString() => $"{BaseName} {Value}{(ReplaceCounters.Any() ? '_' : "")}{string.Join('_', ReplaceCounters)}";
+}
 
 public partial class MainWindowViewModel : ObservableObject
 {
@@ -14,18 +26,35 @@ public partial class MainWindowViewModel : ObservableObject
     [ObservableProperty]
     private bool _enableCollectionSynchronization = false;
 
-    public ObservableRangeCollection<string> ObservableItems { get; } = new();
+    [ObservableProperty]
+    private bool _enableSorting = false;
+
+    [ObservableProperty]
+    private bool _enableFiltering = false;
+
+    [ObservableProperty]
+    private bool _enableGrouping = false;
+
+
+    public ObservableRangeCollection<Item> ObservableItems { get; } = new();
+    public ICollectionView CollectionView { get; }
+
+    public MainWindowViewModel()
+    {
+        CollectionView = CollectionViewSource.GetDefaultView(ObservableItems);
+    }
+
 
     [RelayCommand]
     private void AddItem()
     {
-        ObservableItems.Add($"Item {_itemCounter++}");
+        ObservableItems.Add(new (_itemCounter++));
     }
 
     [RelayCommand]
     private void AddItemsRange()
     {
-        ObservableItems.AddRange(Enumerable.Repeat(() => $"Item {_itemCounter++}", 18).Select(g => g()));
+        ObservableItems.AddRange(Enumerable.Repeat(() => new Item(_itemCounter++), 18).Select(g => g()));
     }
 
     [RelayCommand]
@@ -71,7 +100,7 @@ public partial class MainWindowViewModel : ObservableObject
         {
             return;
         }
-        ObservableItems.Replace(0, ObservableItems[0] + "_" + _replaceCounter);
+        ObservableItems.Replace(0, ObservableItems[0] with { ReplaceCounters = [.. ObservableItems[0].ReplaceCounters, _replaceCounter ] });
     }
 
     [RelayCommand]
@@ -84,25 +113,34 @@ public partial class MainWindowViewModel : ObservableObject
         }
         else if (ObservableItems.Count == 1)
         {
-            ObservableItems.ReplaceRange(0, 1, [ObservableItems[0] + "_" + _replaceCounter]);
+            ObservableItems.ReplaceRange(0, 1, [ObservableItems[0] with { ReplaceCounters = [.. ObservableItems[0].ReplaceCounters, _replaceCounter] }]);
         }
         else if (ObservableItems.Count == 2 || ObservableItems.Count == 3)
         {
-            var newItems = ObservableItems.Take(2).Select(i => i + "_" + _replaceCounter).Concat(["Item_" + _replaceCounter, "Item_" + _replaceCounter + 1]).ToArray();
+            var newItems = ObservableItems.Take(2).Select(i => i with { ReplaceCounters = [..i.ReplaceCounters, _replaceCounter] })
+                .Concat([new Item(_replaceCounter), new Item(_replaceCounter + 1)]).ToArray();
             ObservableItems.ReplaceRange(0, 2, newItems);
         }
         else if (ObservableItems.Count < 20)
         {
             // more removed than new
-            var newItems = ObservableItems.Skip(1).Take(2 + (ObservableItems.Count > 4 ? 1 : 0)).Select(i => i + "_" + _replaceCounter).ToArray();
+            var newItems = ObservableItems.Skip(1).Take(2 + (ObservableItems.Count > 4 ? 1 : 0)).Select(i => i with { ReplaceCounters = [.. i.ReplaceCounters, _replaceCounter] }).ToArray();
             ObservableItems.ReplaceRange(1, 5, newItems);
         }
         else
         {
             // more new than removed
-            var newItems = ObservableItems.Skip(14).Take(5).Select(i => i + "_" + _replaceCounter).ToArray();
+            var newItems = ObservableItems.Skip(14).Take(5).Select(i => i with { ReplaceCounters = [.. i.ReplaceCounters, _replaceCounter] }).ToArray();
             ObservableItems.ReplaceRange(14, 3, newItems);
         }
+    }
+
+    [RelayCommand]
+    private void ClearItems()
+    {
+        ObservableItems.Clear();
+        _itemCounter = 0;
+        _replaceCounter = 0;
     }
 
     partial void OnEnableCollectionSynchronizationChanged(bool value)
@@ -114,6 +152,43 @@ public partial class MainWindowViewModel : ObservableObject
         else
         {
             BindingOperations.DisableCollectionSynchronization(ObservableItems);
+        }
+    }
+
+    partial void OnEnableSortingChanged(bool value)
+    {
+        if (value)
+        {
+            CollectionView.SortDescriptions.Add(new SortDescription(nameof(Item.Value), ListSortDirection.Descending));
+        }
+        else
+        {
+            CollectionView.SortDescriptions.Clear();
+        }
+    }
+
+    partial void OnEnableFilteringChanged(bool value)
+    {
+        if (value)
+        {
+            CollectionView.Filter = item => item is Item itemObj
+                && itemObj.Value % 2 == 0;
+        }
+        else
+        {
+            CollectionView.Filter = null;
+        }
+    }
+
+    partial void OnEnableGroupingChanged(bool value)
+    {
+        if (value)
+        {
+            CollectionView.GroupDescriptions.Add(new PropertyGroupDescription(nameof(Item.Tens)));
+        }
+        else
+        {
+            CollectionView.GroupDescriptions.Clear();
         }
     }
 }
